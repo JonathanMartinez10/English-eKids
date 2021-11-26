a18407b8d25e43aa6550ca252308913302ff8773
Jonathan M <jonathanmartzca@gmail.com>
2021-11-25 23:18:40 -0600

    Primera pantalla 'welcome'

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//Mis importaciones
+import 'package:english_ekids/pages/pages.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'welcome_page',
+      routes:{
+        'welcome_page' : ( _ ) => const WelcomePage(),
+        'login_page'   : ( _ ) => const LoginPage(),
+        'signup_page'  : ( _ ) => const SignUpPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/pages.dart
@@ -0,0 +1,3 @@
+export 'package:english_ekids/pages/welcome_page.dart';
+export 'package:english_ekids/pages/login_page.dart';
+export 'package:english_ekids/pages/signup_page.dart';
