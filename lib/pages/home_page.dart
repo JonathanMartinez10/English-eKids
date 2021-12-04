795ca8aa799b39071a655fe4f55f4b23580c8fa8
PALANTOC31416 <planvdz@gmail.com>
2021-12-03 19:17:14 -0600

    Se agrego el menu de home y sus respectivas imagenes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,8 +12,99 @@ class HomePage extends StatelessWidget {
         title: const Text('English kids'),
         backgroundColor: Colors.deepPurple,
       ),
-      body: const Center(
-        child: Text('Hola Mundo'),
+      body: Container(
+        //Menu de contenido
+        padding: EdgeInsets.all(30.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(image: AssetImage('../assets/home/Alphabet_icon.png')),
+                      SizedBox(height: 80.0,),
+                      Text("Alphabet", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(image: AssetImage('../assets/login/Numbers_icon.png')),
+                      SizedBox(height: 30.0,),
+                      Text("Numbers", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(image: AssetImage('../assets/home/Colors_icon.png')),
+                      SizedBox(height: 80.0,),
+                      Text("Colors", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(image: AssetImage('../assets/home/Shapes_icon.png')),
+                      SizedBox(height: 80.0,),
+                      Text("Shapes", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(image: AssetImage('../assets/home/Body_icon.png')),
+                      SizedBox(height: 80.0,),
+                      Text("Body parts", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            )
+         ],
+        ),
      ),
      bottomNavigationBar: const BottomNavigationBarPrincipal(),
    );
