c4beb90afe36c105340b7767f7feab301a5d1b0d
PALANTOC31416 <planvdz@gmail.com>
2021-12-10 21:43:34 -0600

    actualizacion del main

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'welcome_page',
-      routes:{
-        'welcome_page' : ( _ ) => const WelcomePage(),
-        'login_page'   : ( _ ) => LoginPage(),
-        'signup_page'  : ( _ ) => SignUpPage(),
-        'home_page'    : ( _ ) => const HomePage(),
-        'books_page'   : ( _ ) => const BooksPage(),
-        'videos_page'  : ( _ ) => const VideosPage(),
-        'settings_page': ( _ ) => const SettingsPage(),
+      routes: {
+        'welcome_page': (_) => const WelcomePage(),
+        'login_page': (_) => LoginPage(),
+        'signup_page': (_) => SignUpPage(),
+        'home_page': (_) => const HomePage(),
+        'books_page': (_) => const BooksPage(),
+        'videos_page': (_) => const VideosPage(),
+        'settings_page': (_) => const SettingsPage(),
       },
     );
   }
